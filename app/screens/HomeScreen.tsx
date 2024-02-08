5ef0b4f79240b5ced1e400d7bfc285c25a9ce5e5
Ziyad <mziyadc@gmail.com>
2024-02-08 18:32:01 +0530

    Changed text in home screen

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const HomeScreen = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="font-bold">HomeScreen</Text>
+      <Text className="font-bold">This my life homie, you decide yours</Text>
     </View>
   );
 };
